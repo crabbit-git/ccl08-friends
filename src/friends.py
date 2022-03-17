2d1af25ed0e64af18097e1589185140d16ec8c69
crabbit-git <98990717+crabbit-git@users.noreply.github.com>
2022-03-17 19:42:02 +0000

    Refactored fav food function

--- src/friends.py
+++ src/friends.py
@@ -5,10 +5,12 @@ def get_favourite_tv_show(person):
     return person["favourites"]["tv_show"]
 
 def likes_to_eat(person, food):
-    if food in person["favourites"]["snacks"]:
-        return True
-    else:
-        return False
+    return food in person["favourites"]["snacks"]
+    # This is just shorthand for the following, which also works:
+    # if food in person["favourites"]["snacks"]:
+    #     return True
+    # else:
+    #     return False
 
 def add_friend(person, friend):
     person["friends"].append(friend)
